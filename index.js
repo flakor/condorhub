e11282f19fcb3802c26cbcccdb1f0b4cbe2e5ae9
pablo <pablo.ortiz.barra@gmail.com>
2017-05-12 10:25:59 -0300

    npm scraping zeta

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var morgan     = require('morgan');
+require('rainbow-console');
+var request 	 = require("request");
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,13 +13,20 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+// app.set('view engine', 'html');
 
-app.get('/', function(request, response) {
+app.get('/index', function(request, response) {
   response.render('pages/index');
 });
+
 app.get('/cool', function(request, response) {
   response.send(cool());
 });
+
+app.get('/', function(request, response) {
+  response.render('pages/zeta');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
