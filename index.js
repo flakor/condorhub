775f177e6ea8a1deb5f6990a9f382086ad7aa880
Pablo Ortiz <pablo.ortiz.barra@gmail.com>
2017-10-12 23:28:45 -0300

    elimine la error cheerios

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var morgan     = require('morgan');
 require('rainbow-console');
 var request 	 = require("request");
-var cheerio = require('cheerio');
+
 var preueba = 'preuba';
 app.set('port', (process.env.PORT || 5000));
 
@@ -27,35 +27,7 @@ app.get('/cool', function(request, response) {
   response.send(cool());
 });
 
-app.get('/', function(req, res) {
 
-  var headers = {
-      'User-Agent':       'Super Agent/0.0.1',
-      'Content-Type':     'application/x-www-form-urlencoded'
-  }
-
-  var options = {
-      url: 'http://zeta/informatica/num_datamar/directemar.htm',
-      method: 'GET',
-      headers: headers
-      }
-  request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-      console.log(body);
-      var nombre;
-      var $ = cheerio.load(body);
-      console.log('prueba scraping 1');
-          $('#table1').filter(function(){
-                var data = $(this);
-                // nombre = data.children().first().text();
-                nombre = data.children().text();
-                console.log('prueba scraping 2');
-                console.log(nombre);
-          })
-    }
-  });
-  res.render('pages/zeta');
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
