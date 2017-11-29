5fcdbccb72259f1d5502837421f99173709de83c
Pablo Ortiz <flakor@gmail.com>
2017-11-28 23:34:23 -0300

    make it better2

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,34 @@
 
 
 $('#login').click(function() {
-    swal({
-      title: 'Error!',
-      text: 'Do you want to continue',
-      type: 'error',
-      confirmButtonText: 'Cool'
-    })
+  swal({
+        title: 'Submit Login to run ajax request',
+        input: 'email',
+        showCancelButton: true,
+        confirmButtonText: 'Submit',
+        showLoaderOnConfirm: true,
+        preConfirm: (email) => {
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              if (email === 'taken@example.com') {
+                swal.showValidationError(
+                  'This email is already taken.'
+                )
+              }
+              resolve()
+            }, 2000)
+          })
+        },
+        allowOutsideClick: false
+        }).then((result) => {
+        if (result.value) {
+          console.log(result);
+          swal({
+            type: 'success',
+            title: 'Ajax request finished!',
+            html: 'Submitted email: ' + result.value
+          })
+        }
+        })
 
 })
